44ffb3f4b19624854a4a922cda019e16be1ff287
Joshua Sun <joshuapsun@gmail.com>
2024-11-26 15:02:48 -0500

    I hate rotations

--- turtle.gd
+++ turtle.gd
@@ -1,11 +1,9 @@
 extends Node3D
 class_name Turtle
 
-const TWEEN_LENGTH = 0.5
-const WAIT_TIME = 0.5
+const TWEEN_TIME = 0.5
+const WAIT_TIME = 0.0
 
-var heading: Vector3 = Vector3(1, 0, 0)
-var normal: Vector3 = Vector3(0, 1, 0)
 
 var turtle_actions = []
 var visual_cylinders = []
@@ -13,28 +11,40 @@ var visual_cylinders = []
 func executeMove(distance: float) -> void:
 	var pos_tween = create_tween().set_trans(Tween.TRANS_LINEAR)
 	var current_position = self.position
-	var future_position = current_position + distance * heading
+	var future_position = current_position + distance * self.basis.x
 
-	pos_tween.tween_property(self, "position", future_position, TWEEN_LENGTH)
+	pos_tween.tween_property(self, "position", future_position, TWEEN_TIME)
+	await pos_tween.finished
+
 
 func executeTurn(angle: float) -> void:
+	var heading: Vector3 = self.basis.x
+	var normal: Vector3 = self.basis.y
 	var angle_in_rad = deg_to_rad(angle)
-	var new_heading = self.heading.rotated(normal, angle_in_rad)
-	var new_quaternion_representation = Quaternion(self.heading, new_heading)
-	var heading_tween = create_tween().set_trans(Tween.TRANS_LINEAR)
-	heading_tween.tween_property(self, "quaternion", self.quaternion * new_quaternion_representation, TWEEN_LENGTH)
-	self.heading = new_heading.normalized()
+	var quaternion_rotation = Quaternion(normal, angle_in_rad)
+	var rotation_tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+	rotation_tween.tween_property(self, "quaternion", self.quaternion * quaternion_rotation, TWEEN_TIME)
+	await rotation_tween.finished
 
 func executeRoll(angle: float) -> void:
-	# TODO: WTF? It's not rotating correctly, fix this!
+	var heading: Vector3 = self.basis.x
+	var normal: Vector3 = self.basis.y
 	var angle_in_rad = deg_to_rad(angle)
-	var new_normal = self.normal.rotated(heading, angle_in_rad)
-	var new_quaternion_representation = Quaternion(self.normal, new_normal)
-	var normal_tween = create_tween().set_trans(Tween.TRANS_LINEAR)
-	normal_tween.tween_property(self, "quaternion", self.quaternion * new_quaternion_representation, TWEEN_LENGTH)
-	self.normal = new_normal.normalized()
+	var quaternion_rotation = Quaternion(heading, angle_in_rad)
+	var rotation_tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+	rotation_tween.tween_property(self, "quaternion", self.quaternion * quaternion_rotation, TWEEN_TIME)
+	await rotation_tween.finished
 
-
+func executeDive(angle: float) -> void:
+	var heading: Vector3 = self.basis.x
+	var normal: Vector3 = self.basis.y
+	var left: Vector3 = self.basis.z
+	var angle_in_rad = deg_to_rad(angle)
+	var quaternion_rotation = Quaternion(left, angle_in_rad)
+	var rotation_tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+	rotation_tween.tween_property(self, "quaternion", self.quaternion * quaternion_rotation, TWEEN_TIME)
+	await rotation_tween.finished
+	
 func executeTurtleActions():
 	for action in turtle_actions:
 		if action is MoveAction:
@@ -46,16 +56,19 @@ func executeTurtleActions():
 		elif action is RollAction:
 			var r_action: RollAction = action as RollAction
 			self.executeRoll(r_action.angle)
+		elif action is DiveAction:
+			var d_action: DiveAction = action as DiveAction
+			self.executeDive(d_action.angle)
 		else:
 			print("Action Something went wrong")
 		
-		await get_tree().create_timer(WAIT_TIME).timeout
+		await get_tree().create_timer(WAIT_TIME + TWEEN_TIME).timeout
 
 
 func _ready() -> void:
 	for _i in range(3):
 		self.turtle_actions.append(MoveAction.new(3))
-		self.turtle_actions.append(RollAction.new(120))
+		self.turtle_actions.append(DiveAction.new(120))
 
 	self.executeTurtleActions()
 
@@ -71,6 +84,11 @@ class TurnAction:
 		self.angle = set_angle
 
 class RollAction:
+	var angle: float
+	func _init(set_angle: float = 0.0) -> void:
+		self.angle = set_angle
+
+class DiveAction:
 	var angle: float
 	func _init(set_angle: float = 0.0) -> void:
 		self.angle = set_angle
