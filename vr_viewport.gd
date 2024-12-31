51433bcfaa9b940e5cd716b9026c1ac83bbe7355
Joshua Sun <joshuapsun@gmail.com>
2024-12-31 18:22:00 -0500

    added vr support? (unknown if it works)

---
+++ vr_viewport.gd
@@ -0,0 +1,11 @@
+extends Viewport
+
+var xRInterface: XRInterface
+
+func _init() -> void:
+	XRServer.find_interface("OpenXR")
+	if xRInterface and xRInterface.is_initialized():
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		self.use_xr = true
+	else:
+		print("OpenXR not initialized, running in desktop mode.")
