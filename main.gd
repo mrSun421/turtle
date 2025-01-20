ce0be8bff17e2e16ef05f99f31bff211a9211d92
Joshua Sun <joshuapsun@gmail.com>
2025-01-20 17:46:04 -0500

    added json turtle functionality

--- main.gd
+++ main.gd
@@ -25,12 +25,6 @@ func _ready() -> void:
 	_draw_line(gridMeshInstance, Vector3(0, -MAIN_AXES_SIZE, 0), Vector3(0, MAIN_AXES_SIZE, 0), Color.GREEN)
 	_draw_line(gridMeshInstance, Vector3(0, 0, -MAIN_AXES_SIZE), Vector3(0, 0, MAIN_AXES_SIZE), Color.BLUE)
 
-	for i in range(8):
-		turtle.append_turtle_actions(Turtle.ColorAction.new(Color(0, 1.0 / (8 - i), 0)))
-		turtle.append_turtle_actions(Turtle.MoveAction.new(3))
-		turtle.append_turtle_actions(Turtle.YawAction.new(45))
-
-
 func _draw_line(mesh_instance: MeshInstance3D, pos1: Vector3, pos2: Vector3, color = Color.WHITE_SMOKE):
 	var material := ORMMaterial3D.new()
 	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
--- server.gd
+++ server.gd
@@ -4,6 +4,7 @@ const port = 21327
 
 var server: TCPServer = TCPServer.new()
 var streams: Array[StreamPeerTCP]
+@export var turtle: Turtle
 
 func _init() -> void:
 	var err = server.listen(21327)
@@ -23,8 +24,30 @@ func _process(delta: float) -> void:
 				# var data = stream.get_data(available_bytes)
 				var text_data = stream.get_utf8_string(available_bytes)
 				if text_data:
-					print(text_data)
-					stream.put_data(text_data.to_upper().to_ascii_buffer())
+					var json_data = JSON.parse_string(text_data)
+					for j in len(json_data):
+						var curr_data = json_data[j]
+						match curr_data["type"]:
+							"color":
+								var col_dat = curr_data["color"]
+								var col = Color(col_dat["r"], col_dat["g"], col_dat["b"])
+								turtle.append_turtle_actions(Turtle.ColorAction.new(col))
+							"move":
+								turtle.append_turtle_actions(Turtle.MoveAction.new(curr_data["value"]))
+							"yaw":
+								turtle.append_turtle_actions(Turtle.YawAction.new(curr_data["value"]))
+							"pitch":
+								turtle.append_turtle_actions(Turtle.PitchAction.new(curr_data["value"]))
+							"roll":
+								turtle.append_turtle_actions(Turtle.RollAction.new(curr_data["value"]))
+							"penup":
+								turtle.append_turtle_actions(Turtle.PenUpAction.new())
+							"pendown":
+								turtle.append_turtle_actions(Turtle.PenDownAction.new())
+							_:
+								print("unknown action type: %s" % curr_data["type"])
+					var confirmation_string = "data recieved: %s bytes" % available_bytes
+					stream.put_data(confirmation_string.to_ascii_buffer())
 		else:
 			streams[i].disconnect_from_host()
 			streams.remove_at(i)
@@ -36,8 +59,3 @@ func _process(delta: float) -> void:
 		newStream.set_no_delay(true)
 		streams.append(newStream)
 		print("took connection")
-	
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		var iek_event = event as InputEventKey
