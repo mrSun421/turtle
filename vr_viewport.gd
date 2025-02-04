8d45a64d79ec298adf9c2eb23d070bed0de61e02
Joshua Sun <joshuapsun@gmail.com>
2025-02-03 21:25:06 -0500

    added full vr support

--- vr_viewport.gd
+++ vr_viewport.gd
@@ -1,4 +1,4 @@
-extends Viewport
+extends SubViewport
 
 var xRInterface: XRInterface
 
@@ -6,6 +6,5 @@ func _init() -> void:
 	XRServer.find_interface("OpenXR")
 	if xRInterface and xRInterface.is_initialized():
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-		self.use_xr = true
 	else:
 		print("OpenXR not initialized, running in desktop mode.")
